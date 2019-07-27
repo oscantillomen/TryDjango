9d6fda73a05aa594b2bd9e0dc7720d6c91e3b149
Oscar cantillo <oscantillomen@gmail.com>
2019-07-26 19:32:26 -0500

    first commit

---
+++ products/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    summary = models.TextField(blank=False, null=False)
+    featured = models.BooleanField()
